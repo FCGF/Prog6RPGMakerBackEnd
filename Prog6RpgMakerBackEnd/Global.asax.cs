3f2c45e4c12c73bc878e41c8d42890a5f5b21d26
Fernando Cardoso Garcia Filho <fernando061312@gmail.com>
2017-12-03 18:16:51 -0200

    Adicionado configuração de SimpleInjector

--- Prog6RpgMakerBackEnd/Global.asax.cs
+++ Prog6RpgMakerBackEnd/Global.asax.cs
@@ -2,6 +2,11 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using RpgMaker.Data;
+using RpgMaker.Data.NHibernate;
+using RpgMaker.Service;
+using RpgMaker.ServiceImplementation;
+using SimpleInjector;
 
 namespace Prog6RpgMakerBackEnd {
   public class WebApiApplication : System.Web.HttpApplication {
@@ -11,6 +16,20 @@ namespace Prog6RpgMakerBackEnd {
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+      Container container = new Container();
+
+      container.RegisterSingleton<IAttributeRepository, AttributeRepository>();
+      container.RegisterSingleton<ICharacterSheetRepository, CharacterSheetRepository>();
+      container.RegisterSingleton<IChronicleRepository, ChronicleRepository>();
+      container.RegisterSingleton<IGameCharacterRepository, IGameCharacterRepository>();
+      container.RegisterSingleton<IPlayerRepository, PlayerRepository>();
+      container.RegisterSingleton<IUserRepository, UserRepository>();
+
+      container.RegisterSingleton<IAttributeService, AttributeService>();
+
+      container.Verify();
+
     }
   }
 }
